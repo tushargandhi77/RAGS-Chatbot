f03c97acdbc8886ea83805883d97dee368b17dfe
Tushar Gandhi <104029815+tushargandhi77@users.noreply.github.com>
2025-04-10 10:42:32 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
